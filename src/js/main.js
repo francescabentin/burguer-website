685487b97864ecbab273f18aa3cadba8aae0725c
Francesca Bentin <francescabentin@gmail.com>
2023-06-07 15:31:34 +0200

    add logo and navbar with toggleEvent

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const wholeMenu = document.querySelector('.js-menu');
+const menuIcon = document.querySelector('.js-icon');
+const menuItems = document.querySelectorAll('.header__menu__nav__ul li a');
+const closeIcon = document.querySelector(".header__menu__nav__ul__li__close");
+
+function handleClickEvent() {
+    wholeMenu.classList.toggle("show");
+}
+
+menuIcon.addEventListener("click", handleClickEvent);
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        wholeMenu.classList.remove("show");
+    });
+});
+
+closeIcon.addEventListener('click', () => {
+    wholeMenu.classList.remove("show");
+})
+
+
