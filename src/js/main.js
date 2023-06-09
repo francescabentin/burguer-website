30b381e469d4db8e64042a60413d7238c725b303
Francesca Bentin <francescabentin@gmail.com>
2023-06-09 18:30:35 +0200

    add about section

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,10 @@ const menuIcon = document.querySelector('.js-icon');
 const menuItems = document.querySelectorAll('.header__menu__nav__ul li a');
 const closeIcon = document.querySelector(".header__menu__nav__ul__li__close");
 
+
 function handleClickEvent() {
     wholeMenu.classList.toggle("show");
 }
-
 menuIcon.addEventListener("click", handleClickEvent);
 
 menuItems.forEach(item => {
@@ -20,5 +20,19 @@ menuItems.forEach(item => {
 closeIcon.addEventListener('click', () => {
     wholeMenu.classList.remove("show");
 })
+
+const slider = document.querySelector('.js-slider');
+const images = slider.querySelectorAll('.js-img');
+let currentImageIndex = 0;
+
+function showNextImage() {
+    images[currentImageIndex].style.display = 'none';
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.display = 'block';
+}
+
+setInterval(showNextImage, 2000);
+
+images[currentImageIndex].style.display = 'block';
 
 
